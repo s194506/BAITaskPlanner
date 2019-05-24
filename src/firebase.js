10cfb76db1ae0d8e29dda28e8ac2c3f505de6626
s194506 <32495171+s194506@users.noreply.github.com>
2019-05-24 12:03:50 +0200

    Add files via upload
    dodanie pliku konfiguracyjnego Firebase

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase';
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDshipfidHOJfkws_TwlTVmATnVMdp0Blw",
+  authDomain: "taskplanner-62e04.firebaseapp.com",
+  databaseURL: "https://taskplanner-62e04.firebaseio.com",
+  projectId: "taskplanner-62e04",
+  storageBucket: "taskplanner-62e04.appspot.com",
+  messagingSenderId: "891181347622",
+  appId: "1:891181347622:web:ec82ef9d11549ef8"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+
+window.firebase = firebase;
+
+
+
+export default firebase;
