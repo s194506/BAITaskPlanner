2e440aed75ab1a169e2fb7118da2327468819ed9
Dz0702 <30319035+Den0702@users.noreply.github.com>
2019-05-30 22:37:27 +0200

    Dodanie folderów+zadań+kolaborantów
    Dodanie możliwości tworzenia folderów+edytowania folderów+przydzielenie kolaborantów do folderów+możliwość tworzenia zadań

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import MainPage from './MainPage';
 import SignInPage from './SignInPage';
 import SignUpPage from './SignUpPage';
 import FolderListPage from './FolderListPage';
+import CreateFolderPage from './CreateFolderPage';
 import FolderViewPage from './FolderViewPage';
 import ChatPage from './ChatPage';
 import firebase from 'firebase';
+import EditFolderPage from './EditFolderPage';
 
 export default class App extends React.Component {
 
@@ -26,8 +28,10 @@ export default class App extends React.Component {
         <Route exact path='/' component={MainPage}/>
         <Route path='/signin' component={SignInPage}/>
         <Route path='/signup' component={SignUpPage}/>
+        <Route path='/folders/create' component={CreateFolderPage}/>
         <Route path='/folders/:folderId/chat' component={ChatPage}/>
-        <Route path='/folders/:folderId' component={FolderViewPage}/>
+        <Route path='/folders/:folderId/edit' component={EditFolderPage}/>
+        <Route path='/folders/:folderId/tasks' component={FolderViewPage}/>
         <Route path='/folders' component={FolderListPage}/>
       </Switch>      
     );
